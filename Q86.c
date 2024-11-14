b47bbba6d8f34d05839f1348b2adc5da66922224
OjashGupta03 <ojashgupta03@gmail.com>
2024-11-14 14:48:22 +0530

    Add files via upload

---
+++ Q86.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int strlen1(char a[100]);
+int main()
+{
+    char a[100];
+    printf("read the line:");
+    gets(a);
+    strlen1(a);
+}
+int strlen1(char a[100])
+{
+    int i=0;
+    while(a[i++]!='\0');
+    i--;
+    printf("len of string=%d",i); 
+}
